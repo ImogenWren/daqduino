b2f4a8f6ed5f73af7c8e0f634dd443f625b757a0
ImogenWren <imogen.wren91@gmail.com>
2023-05-23 17:00:58 +0100

    Started work on desktop app using python

---
+++ daqduino-app/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+DAQduino Interface
+
+- Open Serial commuinication port with arduino running DAQduino firmware
+- Show live data streamed from DAQduino
+- Display Data on Graphs
+- Save Data to CSV File
+
+"""
+
+__author__ = "Imogen Heard"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def main():
+    print("Starting DAQduino CLI")
+
+
+
+
+if __name__ == "__main__":
+    main()
