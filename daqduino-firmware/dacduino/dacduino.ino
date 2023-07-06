13cab7a3f6a004020c77387347bb73ef711351fe
ImogenWren <imogen.wren91@gmail.com>
2023-07-06 17:01:17 +0100

    bdfdfbdb

--- daqduino-firmware/dacduino/dacduino.ino
+++ daqduino-firmware/dacduino/dacduino.ino
@@ -17,6 +17,45 @@
 */
 
 
+/*  Scaling
+
+          Volts per ADC = 4.89 mV
+                        = 0.00489 V
+
+          DAQduino internal amplifier is set up for 
+
+          20 mA ~= 4.4 V output
+
+          Therefore:
+
+          scale:
+
+          mA = 20/4.4 V
+          mA = 4.54 V
+*/
+
+/*  First Test
+
+          To account for DC offset in readings, measure difference between multimeter readings and DAQduino reporting.
+          To Start: 
+|         | ADC   |  Vreported  | Vmeasured  | Vdiff    | % diff   | mA reported | mA measured  | mA diff |
+|---      |---    |---          |---         |---       |---       |---          |---           |---      |
+| Lowest  | 162   |   0.792     |   0.772    |  0.02    |          | 3.596       | 2.11         | 1.486   |
+|MidRange | 400   |   1.956     |   1.888    |  0.068   |          | 8.880       | 20.69        |-11.81   |  
+| Highest | 632   |   3.090     |   2.977    |  0.113   |          | 14.031      | 23.47        |-9.439   |
+|---      |---    |---          |---         |---       |---       |---          |---           |         |
+
+
+*/
+
+//#define ADC_VOLTS_SCALER 0.0048876  // Value Gained via maths for ADC scale calculation
+//#define ADC_VOLTS_SCALER 0.0047341     // Value taking into account 3.14% average difference between top and bottom values
+//#define ADC_VOLTS_SCALER 0.0045854     // wwinging it at this point (too low)
+//#define ADC_VOLTS_SCALER 0.0046969       // still slightly too low
+#define ADC_VOLTS_SCALER   0.0047339 
+
+#define VOLT_AMP_SCALER 4.5
+
 #define VOLTAGE_SENSE_PIN A2
 
 // Use 1.074 for 1.1v reference
@@ -24,41 +63,40 @@
 // Use 4.883 for 5v reference
 //#define ADC_TO_MV_SCALER 4.883
 
-#define ADC_VOLTS_SCALER 1.0
 
-#define VOLT_AMP_SCALER  
 
 void setup() {
   Serial.begin(115200);
+  Serial.println("\n DAQduino - Data AQuisition System");
   //analogReference(INTERNAL);
-
 }
 
 void loop() {
 
-  char buffer[64];                                          // Buffer to hold printed values
+  char buffer[64];  // Buffer to hold printed values
 
-  int adc_value = analogRead(VOLTAGE_SENSE_PIN);                // Take Reading
+  int adc_value = analogRead(VOLTAGE_SENSE_PIN);  // Take Reading
 
-  float mV_float =  float(adc_value) * ADC_VOLTS_SCALER;     // Calculate mV measured by ADC
+  float V_float = float(adc_value) * ADC_VOLTS_SCALER;  // Calculate mV measured by ADC
 
-  float mA_float = mV_float / SHUNT_RESISTOR;                // Calculate mA through shunt resistor
+
+  float mA_float = (V_float * VOLT_AMP_SCALER );  // Calculate mA through shunt resistor
 
   // Strings for converting floats for printing
-  char mV_str[6];
+  char V_str[16];
+  char mA_str[16];
 
-  char mA_str[6];
+  // Convert floats to string as arduino does not like floats in sprintf
+  dtostrf(V_float, 4, 3, V_str);
+  dtostrf(mA_float, 4, 3, mA_str);
 
-// Convert floats to string as arduino does not like floats in sprintf
-  dtostrf(mV_float, 4, 2, mV_str);
-  dtostrf(mA_float, 4, 2, mA_str);
 
-// Place all data into a single buffer & format for printing
-  sprintf(buffer, "ADC: %4i, mV: %s, mA: %s ", adc_value, mV_str, mA_str);
+  // Place all data into a single buffer & format for printing
+  sprintf(buffer, "ADC: %4i  |  V: %s  |  mA: %s ", adc_value, V_str, mA_str);
 
-// Print the buffer
+  // Print the buffer
   Serial.println(buffer);
 
-// Delay to slow down reporting for human readability
+  // Delay to slow down reporting for human readability
   delay(500);
 }
